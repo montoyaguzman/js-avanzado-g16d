b7d7afea2e9ceb1c36302fb6c4b113d34817c8de
montoyaguzman <montoyaguzman7@gmail.com>
2022-07-06 21:34:15 -0500

    feat: ejemplo basicos de exports

---
+++ 1-node/1.4-export/functions.js
@@ -0,0 +1,20 @@
+function sum(a, b) {
+    return a + b;
+}
+
+function substract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+exports.sum = sum;
+exports.substract = substract;
+exports.multiply = multiply;
+
---
+++ 1-node/1.4-export/index.js
@@ -0,0 +1,10 @@
+const operations = require('./functions');
+
+function main() {
+    let numero1 = 10;
+    let numero2 = 40;
+    console.log('suma: ', operations.sum(numero1, numero2));
+    console.log('division: ', operations.divide(numero1, numero2));
+}
+
+main();
