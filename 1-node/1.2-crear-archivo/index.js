389ab2b9ef82bd556be439201a7454fbf7753282
montoyaguzman <montoyaguzman7@gmail.com>
2022-07-06 20:39:52 -0500

    feat: ejemplo de ejecucion desde code runner

--- 1-node/1.2-crear-archivo/index.js
+++ 1-node/1.2-crear-archivo/index.js
@@ -3,12 +3,17 @@ const fs = require('fs');
 // const patito = require('fs');
 // patito.writeFile('my-file.txt', 'Este texto lo escribi desde node');
 
-const fileName = './my-file.txt';
+// code runner - se ejecuta en la raiz del proyecto
+const fileName = './1-node/1.2-crear-archivo/my-file.txt';
+
+// node index.js - se ejecuta dentro de la carpeta 1.2-crear-archivo
+// const fileName = './my-file.txt';
+
 const textContent = 'Este texto lo escribi desde node';
 fs.writeFile(fileName, textContent, (err) => {
     if (err) {
         throw err;
     } else {
-        console.log('¡archivo creado con extito!');
+        console.log('¡archivo creado con éxito!');
     }
 });
