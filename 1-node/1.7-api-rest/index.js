402155288cff99b6bb6f64ea47185bdcef373714
montoyaguzman <montoyaguzman7@gmail.com>
2022-07-11 21:31:39 -0500

    feat: estructura de proyectos finales de node

---
+++ 1-node/1.7-api-rest/index.js
@@ -0,0 +1,7 @@
+// PETICIONES
+// 200 OK
+// 304 Not modified
+// 404 Not Found
+// 500 Error del servidor
+// 408 Time out
+// 403 Forbidden
---
+++ 1-node/1.8-media-server/index.js
@@ -0,0 +1,4 @@
+// PETICIONES
+// localhost:8080/index.html
+// localhost:8080/pagina1.html
+// localhost:8080/foto1.jpg
