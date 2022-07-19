75339fc26650d4b23efa5ca0550f33bcfee008ab
montoyaguzman <montoyaguzman7@gmail.com>
2022-07-18 20:07:39 -0500

    feat: primero ejemplo async await

--- 2-asincronia/2.4-async-await.js
+++ 2-asincronia/2.4-async-await.js
@@ -6,26 +6,42 @@
 //     return 44;
 // }
 
-const promiseValidandoPago = async () => {
-    // El codigo asincrono de validandoPago debe quedar dentro de la promise
-    // setTimeout(() => {
-    //     // Este setTimeout representa una operacion que depende de otra entidad
-    //     // en este caso simulas esa dependencia con tiempo
-    //     console.log('2 Revisando con el banco y mostrando un loader...');
-    //     // if (status === 200) {
-    //     //     resolve();
-    //     // } else {
-    //     //     reject(new Error('Este es mi custom error'));
-    //     // }
-    // })
-    await console.log('2 Revisando con el banco y mostrando un loader...');
+// const promiseValidandoPago = async () => {
+//     // El codigo asincrono de validandoPago debe quedar dentro de la promise
+//     setTimeout(() => {
+//         // Este setTimeout representa una operacion que depende de otra entidad
+//         // en este caso simulas esa dependencia con tiempo
+//         console.log('2 Revisando con el banco y mostrando un loader...');
+
+//         // if (status === 200) {
+//         //     resolve();
+//         // } else {
+//         //     reject(new Error('Este es mi custom error'));
+//         // }
+//     })
+// }
+const promiseValidandoPago = () => {
+    return new Promise((resolve, reject) => {
+        // El codigo asincrono de validandoPago debe quedar dentro de la promise
+        setTimeout(() => {
+            // Este setTimeout representa una operacion que depende de otra entidad
+            // en este caso simulas esa dependencia con tiempo
+            console.log('2 Revisando con el banco y mostrando un loader...');
+            resolve('todo gucci!!');
+            // if (status === 200) {
+            //     resolve();
+            // } else {
+            //     reject(new Error('Este es mi custom error'));
+            // }
+        }, 0);
+    })
+};
+
+async function main() {
+    console.log('1 Presiona el boton comprar'); 
+    const aux = await promiseValidandoPago();
+    console.log(aux);
+    console.log('3 Tu paquete sera enviado :)');
 }
 
-console.log('1 Presiona el boton comprar'); 
-promiseValidandoPago().then(() => console.log('3 Tu paquete sera enviado :)') )
-    .then(() => console.log('4 Gracias por tu compra'))
-    .then(() => console.log('5 Avisando al proveedor'))
-    .catch((error) => {
-        console.log('Mostrando un modal con el error: ', error);
-    })
-    .finally(() => console.log('==== FINAL (cerrando loader) ===='))
+main();
