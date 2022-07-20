6e1331867ebb82468397096f8f621be56e08e8ef
montoyaguzman <montoyaguzman7@gmail.com>
2022-07-19 21:12:28 -0500

    feat: modificaciones de gets y agrega post

--- 3-express-js/3.1-shoes-api/index.js
+++ 3-express-js/3.1-shoes-api/index.js
@@ -3,39 +3,49 @@ const colors = require('colors');
 const app = express();
 const port = 3000;
 
+app.use(express.json());
+
 app.listen(port, () => {
     console.log('Servidor express listening...'.rainbow);
 });
+
+let shoes = [
+    { id: 1, brand: 'noke', price: 200, size: 29 },
+    { id: 2, brand: 'edidas', price: 500, size: 22 },
+    { id: 3, brand: 'floxi', price: 900, size: 25 },
+];
 
 app.get('/', (req, res) => {
     res.send('hola mundo!');
 });
 
+// http://localhost:3000/shoes/
+// Query Params: Filtrar información
+// http://localhost:3000/shoes/?page=1&pageSize=10&brand=%22noke%22
+// %20 => espacio en blanco
+// %22 => comillas dobles
 app.get('/shoes', (req, res) => {
-    const shoes = [
-        { id: 1, brand: 'noke', price: 200, size: 29 },
-        { id: 2, brand: 'edidas', price: 500, size: 22 },
-        { id: 3, brand: 'floxi', price: 900, size: 25 },
-    ];
+    const { page, pageSize, brand } = req.query;
+    if (page && pageSize && brand) {
+        res.json({ page, pageSize, brand });
+    } 
     res.json(shoes);
 });
 
 // Request Param: Son utilizados para ejecutar operaciones sobre un elemento especifico
+// http://localhost:3000/shoes/999
 app.get('/shoes/:id', (req, res) => {
-    const id = req.params.id;
+    const { id } = req.params;
     const shoe = { id: 1, brand: 'noke', price: 200, size: 29, searching: id };
     res.json(shoe);
 });
 
-// Query Params: Filtrar información
-// http://localhost:3000/shoes/?page=1&pageSize=10&brand='noke'
-app.get('/shoes', (req, res) => {
-    const page = req.query.page;
-    const pageSize = req.query.pageSize;
-    const brand = req.query.brand;
-    if (page && pageSize) {
-        res.json({ page: page, pageSize: pageSize, brand: brand });
-    } else {
-        res.send('no enviaste query params');
-    }
+// http://localhost:3000/shoes/
+// { id: 4, brand: 'pima', price: 600, size: 26 }
+app.post('/shoes', (req, res) => {
+    const newShoe = req.body;
+    shoes.push(newShoe);
+    console.log(shoes);
+    const response = { message: 'shoe created!'};
+    res.status(201).json(response);
 });
